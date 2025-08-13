52f5bb30f5cad5635e18173b2535c2044e876039
CatbyteHD <m66361544@gmail.com>
2025-08-13 16:01:21 +0300

    Update BreezeHub.lua

--- BreezeHub.lua
+++ BreezeHub.lua
@@ -10,7 +10,7 @@ DisableRayfieldPrompts = false,
 DisableBuildWarnings = false,
 ConfigurationSaving = {
 Enabled = true,
-FolderName = "GubbyHubOfficial",
+FolderName = "BreezeHub",
 FileName = "Settings"
 },
 Discord = {
