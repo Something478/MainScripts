f5817e92b386eb0ea0f6e79f66d4915ea289499c
ChillbyteHD <m66361544@gmail.com>
2025-08-14 10:09:20 +0300

    Update BreezeHub.lua

--- BreezeHub.lua
+++ BreezeHub.lua
@@ -1,5 +1,47 @@
-local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
+--- // ==== Blacklist ==== \\ ---
+local Players = game:GetService("Players")
 
+local function notif(str, dur)
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Service";
+        Text = str;
+        Duration = dur or 3;
+    })
+end
+
+local TARGET_USERNAMES = loadstring(game:HttpGet("https://raw.githubusercontent.com/Something478/MyScripts/refs/heads/main/Blacklist.lua"))()
+
+local TAG_NAME = "Blacklisted"
+local TargetLookup = {}
+
+for _, name in pairs(TARGET_USERNAMES) do
+    TargetLookup[name] = true
+end
+
+local function Blacklist(player)
+    if player.Character then
+        if player.Character:FindFirstChild("Head") and player.Character.Head:FindFirstChild(TAG_NAME) then
+            return
+        end
+        game:GetService("StarterGui"):SetCore("SendNotification", {
+            Title = "From ChillbyteHD";
+            Text = "You Are Blacklisted by the owner, sorry! :(";
+            Duration = 3;
+        })
+    end
+end
+
+for _, player in pairs(Players:GetPlayers()) do
+    if TargetLookup[player.Name] then
+        Blacklist(player)
+        return
+    end
+end
+
+--- // ==== DevTag ==== \\ ---
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Something478/MyScripts/refs/heads/main/DevTag.lua"))()
+
+--- // ==== Rayfield ==== \\ ---
 local Window = Rayfield:CreateWindow({
 Name = "Breeze hub",
 LoadingTitle = "Breeze hub",
