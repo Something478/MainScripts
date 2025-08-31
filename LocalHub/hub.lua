1d8393f9ce008236340ff40906b329269f3c7cda
ChillbyteHD <m66361544@gmail.com>
2025-08-31 10:06:06 +0300

    update LocalHub/hub.lua

--- LocalHub/hub.lua
+++ LocalHub/hub.lua
@@ -548,10 +548,11 @@ PdTab:CreateParagraph({
     })
   
   PdTab:CreateButton({
-    Name = "Gelatek Reanimation (Creds to Gelatekussy)"
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Something478/ScriptLoader/refs/heads/main/GelatekReanimation.lua"))()
-       end
-   })
+    Name = "Gelatek Reanimation (Creds to Gelatekussy)" 
+    Callback = function()
+           loadstring(game:HttpGet("https://raw.githubusercontent.com/Something478/ScriptLoader/refs/heads/main/GelatekReanimation.lua"))()
+        end
+    })
 
 -- /// Genesis tab
 
